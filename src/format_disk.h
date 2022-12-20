6c4f0a10d4a029c2c4e40526ac29574d1cb349f0
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2022-12-20 17:30:31 +0100

    Update format_disk.h

--- src/format_disk.h
+++ src/format_disk.h
@@ -8,7 +8,7 @@ void format_disk(string disk){
     format_disk.open("src/diskpart_scripts/format_disk", ios::out);
     format_disk << script;
     format_disk.close();
-    cout << "Are you sure you want to restore the disk (y/n)";
+    cout << "\n Are you sure you want to restore the disk (y/n)";
     cout << "\n\n -> ";
     cin >> sure;
     if(sure == "y" || "Y"){
